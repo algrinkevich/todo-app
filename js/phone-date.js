a5ca029ba387d545e8973b9476643b5cdc1b3aa8
agrinkevich <alina.grinkevich@quantori.academy>
2023-03-14 16:59:54 +0600

    TASK-1: Refactor to use 1 component for date and time

--- js/phone-date.js
+++
@@ -1,9 +0,0 @@
-function displayCurrentDate() {
-    const screenDates = document.querySelectorAll("[data-phone-date]");
-    for (let date of screenDates) {
-        date.textContent = moment().format('dddd, DD MMMM');
-    }
-}
-
-displayCurrentDate();
-setInterval(displayCurrentDate, 1000);
--- js/phone-time.js
+++
@@ -1,9 +0,0 @@
-function displayCurrentTime() {
-    const screenTimes = document.querySelectorAll("[data-phone-time]");
-    for (let time of screenTimes) {
-        time.textContent = moment().format('HH:mm'); 
-    }
-}
-
-displayCurrentTime();
-setInterval(displayCurrentTime, 1000);
