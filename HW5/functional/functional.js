cea804b52c92c0caee0edcd81e6aa37658723fe1
agrinkevich <alina.grinkevich@quantori.academy>
2023-04-07 18:38:10 +0300

    TASK-5: Add components for general layout and Modal
    The following components are added:
    - Header;
    - Search;
    - Modal;
    - Input text;
    - Overlay

--- HW5/functional/functional.js
+++ HW5/functional/functional.js
@@ -23,7 +23,7 @@
      * @param items {string[]}
      * @returns {HTMLElement} - List element
      */
-    function List({items}) {
+     function List({items}) {
         const listItems = items.map((item) => `<li>${item}</li>`).join("");
         const ul = document.createElement("ul");
         ul.innerHTML = listItems;
@@ -43,22 +43,102 @@
         return button;
     }
 
+    //Header component
+    function Header({text, level}) {
+        const header = document.createElement(`h${level || 1}`);
+        header.innerHTML = text;
+        return header;
+    }
+
+    //Search component
+    function Search({placeholder}) {
+        const search = document.createElement("input");
+        search.setAttribute("type", "search");
+        search.setAttribute("placeholder", placeholder);
+        return search;
+    }
+
+    function InputText({placeholder}) {
+        const input = document.createElement("input");
+        input.setAttribute("placeholder", placeholder);
+        return input;
+    }
+
+    //Modal component
+    function Modal(overlay) {
+        const div = document.createElement("div"); 
+
+        function closeModal() {
+            div.classList.add("hidden");
+            overlay.classList.add("hidden");
+        }
+
+        div.classList.add("modal", "hidden");
+        const header = Header({text: "Add New Task"});
+        const inputModal = InputText({placeholder: "Task Title"});
+        const cancelButton = Button({text: "Cancel", onClick: closeModal});
+        const addButton = Button({text: "Add Task"});
+        div.append(header, inputModal, cancelButton, addButton);
+        return div;
+    }
+
+    function Overlay() {
+        const div = document.createElement("div");
+        div.classList.add("overlay", "hidden");
+        return div;
+    }
 
     /**
      * App container
      * @returns {HTMLDivElement} - The app container
      */
     function App() {
-        const [items, setItems] = useState(["Item 1", "Item 2", "Item 3"]);
+        const [items, setItems] = useState(["Task 1 Title", "Task 2 Title", "Task 3 Title"]);
+        const [completedItems, setCompletedItems] = useState([
+            "Completed Task 1 Title", 
+            "Completed Task 2 Title", 
+            "Completed Task 3 Title"
+        ]);
 
         function addItem() {
-            setItems([...items, `Item ${items.length + 1}`]);
+            setItems([...items, `Task ${items.length + 1} Title`]);
         }
 
+        function addCompletedItem() {
+            setCompletedItems([...completedItems, `Completed Task ${completedItems.length + 1} Title`]);
+        }
+
+        function openModal() {
+            modal.classList.remove("hidden");
+            overlay.classList.remove("hidden");
+        }
+
+
         const div = document.createElement("div");
-        const list = List({items});
-        const button = Button({text: "Add item", onClick: addItem});
-        div.append(list, button);
+        const header = Header({text: "To Do List", level: 1})
+        const search = Search({placeholder: "Search Task"});
+        const taskButton = Button({text: "+ New Task", onClick: openModal});
+        const allTasksHeader = Header({text: "All Tasks", level: 2});
+        const unfinishedList = List({items});
+        const itemButton = Button({text: "Add item", onClick: addItem});
+        const completedTasksHeader = Header({text: "Completed Tasks", level: 2});
+        const finishedList = List({items: completedItems});
+        const overlay = Overlay();
+        const modal = Modal(overlay);
+   
+
+        div.append(
+            header, 
+            search, 
+            taskButton, 
+            allTasksHeader, 
+            unfinishedList, 
+            itemButton, 
+            completedTasksHeader, 
+            finishedList,
+            modal,
+            overlay
+        );
         return div;
     }
 
