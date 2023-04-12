41b2eabbda5d5233a5a8d067a64983b0115d7894
agrinkevich <alina.grinkevich@quantori.academy>
2023-04-12 16:43:38 +0300

    TASK-5: Rewrite the app from function components to OOP

--- HW5/oop/app.js
+++ HW5/oop/app.js
@@ -2,27 +2,158 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            items: ['item1', 'item2', 'item3']
+            allTasks: ["Task 1 Title", "Task 2 Title", "Task 3 Title"],
+            completedTasks: [
+                "Completed Task 1 Title",
+                "Completed Task 2 Title",
+                "Completed Task 3 Title",
+            ],
+            searchQuery: "",
+            lastAction: null,
+            showPopup: false,
         };
     }
 
     render(props) {
-        console.log(this.state.items)
+        console.log(this.state.items);
+
+        const deleteTask = (title) => {
+            this.setState({
+                ...this.state,
+                lastAction: "Delete Task",
+                allTasks: this.state.allTasks.filter((task) => task !== title),
+            });
+        };
+
+        const addNewTask = (title) => {
+            this.setState({
+                ...this.state,
+                lastAction: "Add Task",
+                allTasks: [...this.state.allTasks, title],
+                showPopup: false
+            });
+        }
+
+        const addCompletedTask = (title) => {
+            this.setState({
+                ...this.state,
+                lastAction: "Complete Task",
+                allTasks: this.state.allTasks.filter((task) => task !== title),
+                completedTasks: [...this.state.completedTasks, title],
+            });
+        };
+
+        const updateQuery = (query) => {
+            this.setState({
+                ...this.state,
+                lastAction: "Search Query",
+                searchQuery: query,
+            });
+        };
+
+        const showPopup = () => {
+            this.setState({
+                ...this.state,
+                showPopup: true
+            })
+        };
+
+        const hidePopup = () => {
+            this.setState({
+                ...this.state,
+                showPopup: false
+            })
+        };
+
+        const children = [
+            new Header({ level: 1 }).render({
+                text: "To Do List",
+                styleClasses: ["app-wraper__header"],
+            }),
+
+            new TopPanel().render({
+                children: [
+                    new Search().render({
+                        placeholder: "Search Task",
+                        onSearch: updateQuery,
+                        query: this.state.searchQuery,
+                        isFocused: this.state.lastAction === "Search Query",
+                    }),
+                    new Button().render({
+                        text: "+ New Task",
+                        onClick: showPopup,
+                        styleClasses: [
+                            "new-task-btn",
+                            "top-panel__new-task-btn",
+                            "confirm-btn",
+                        ],
+                    }),
+                ],
+            }),
+
+            new TasksSection().render({
+                children: [
+                    new Header({ level: 2 }).render({
+                        text: "All Tasks",
+                        styleClasses: ["tasks-section__subheader"],
+                    }),
+                    new List().render({
+                        //items: createTasks(),
+                        items: this.state.allTasks
+                            .filter((title) =>
+                                title
+                                    .toLowerCase()
+                                    .includes(
+                                        this.state.searchQuery.toLowerCase()
+                                    )
+                            )
+                            .map((title) =>
+                                new Task().render({
+                                    title: title,
+                                    onDelete: deleteTask,
+                                    onComplete: addCompletedTask,
+                                })
+                            ),
+                        styleClasses: "task-section",
+                    }),
+                    new Header({ level: 2 }).render({
+                        text: "Completed Tasks",
+                        styleClasses: ["tasks-section__subheader"],
+                    }),
+                    new List().render({
+                        // items: createTasks(),
+                        items: this.state.completedTasks.map((title) =>
+                            new CompletedTask().render({
+                                title: title,
+                            })
+                        ),
+                        styleClasses: "task-section",
+                    }),
+                ],
+            }),
+        ];
+        if (this.state.showPopup) {
+            children.push(new PopupContainer().render({
+                onCancel: hidePopup,
+                onClickAdd: addNewTask
+            }));
+        }
         return super.render({
-            children: [
-                new List().render({
-                    items: this.state.items,
-                    addItem: this.addItem
-                })
-            ]
+            children: [new AppWrapper().render({
+                children
+            })],
+            styleClasses: ["app-container"]
         });
     }
 
     addItem = () => {
         this.setState({
-            items: [...this.state.items, 'item' + (this.state.items.length + 1)]
+            items: [
+                ...this.state.items,
+                "item" + (this.state.items.length + 1),
+            ],
         });
-    }
+    };
 }
 
 document.body.appendChild(new App().render());
--- HW5/oop/components.js
+++ HW5/oop/components.js
@@ -1,8 +1,7 @@
 class Button extends Component {
     constructor() {
         super();
-        this.element = document.createElement('button');
-        this.element.style.cssText = 'color: red;';
+        this.element = document.createElement("button");
     }
 
     /**
@@ -13,18 +12,33 @@ class Button extends Component {
      * @returns {HTMLElement}
      */
     render(props) {
+        if (props.type) {
+            this.element.type = props.type;
+        }
+        if (props.enabled === false) {
+            this.element.disabled = "true";
+        }
         return super.render({
             onClick: props.onClick,
             children: props.text,
-            style: this.state.style
+            styleClasses: props.styleClasses,
         });
+    }
+}
+
+class AppWrapper extends Component {
+    render(props) {
+        return super.render({
+            children: props.children,
+            styleClasses: ["app-wrapper"]
+        })
     }
 }
 
 class List extends Component {
     constructor() {
         super();
-        this.element = document.createElement('ul');
+        this.element = document.createElement("ul");
     }
 
     /**
@@ -36,14 +50,11 @@ class List extends Component {
      */
     render(props) {
         return super.render({
-            children: [...props.items.map(item => {
-                return new ListItem().render({children: item});
-            }),
-                new Button().render({
-                    text: 'Add item',
-                    onClick: props.addItem,
-                })
-            ]
+            children: [
+                ...props.items.map((item) => {
+                    return new ListItem().render({ children: [item] });
+                }),
+            ],
         });
     }
 }
@@ -51,7 +62,271 @@ class List extends Component {
 class ListItem extends Component {
     render(props) {
         return super.render({
-            children: props.children
+            children: props.children,
+        });
+    }
+}
+
+class Header extends Component {
+    constructor({ level }) {
+        super();
+        this.element = document.createElement(`h${level}`);
+    }
+
+    render(props) {
+        return super.render({
+            children: [props.text],
+            styleClasses: props.styleClasses,
+        });
+    }
+}
+
+class InputText extends Component {
+    constructor() {
+        super();
+        this.element = document.createElement(`input`);
+    }
+
+    render(props) {
+        this.element.oninput = props.onInput;
+        this.element.name = props.name;
+        this.element.type = props.type;
+        this.element.value = props.value;
+        this.element.autocomplete = "off";
+        this.element.placeholder = props.placeholder;
+        this.element.classList.add("input-text");
+        if (props.setFocus) {
+            setTimeout(() => {
+                this.element.focus();
+            }, 0);
+        }
+        return super.render({
+            children: [],
+        });
+    }
+}
+
+class Search extends InputText {
+    render(props) {
+        return super.render({
+            placeholder: props.placeholder,
+            onInput: () => props.onSearch(this.element.value),
+            name: "search",
+            type: "search",
+            value: props.query,
+            setFocus: props.isFocused,
+        });
+    }
+}
+
+class TopPanel extends Component {
+    render(props) {
+        return super.render({
+            children: props.children,
+            styleClasses: ["top-panel"],
+        });
+    }
+}
+
+class TasksSection extends Component {
+    render(props) {
+        return super.render({
+            children: props.children,
+            styleClasses: ["tasks-section"],
+        });
+    }
+}
+
+class CompletedTask extends Component {
+    render(props) {
+        return super.render({
+            children: [
+                new DisabledCheckbox().render({
+                    title: props.title,
+                }),
+                new Label().render({
+                    title: props.title,
+                    styleClasses: [
+                        "task-row__title",
+                        "task-row__title--completed",
+                    ],
+                }),
+            ],
+            styleClasses: ["task-row"],
+        });
+    }
+}
+
+class Task extends Component {
+    render(props) {
+        return super.render({
+            children: [
+                new Checkbox().render({
+                    title: props.title,
+                    onChecked: props.onComplete,
+                }),
+                new Label().render({
+                    title: props.title,
+                    styleClasses: ["task-row__title"],
+                }),
+                new DeleteIcon().render({
+                    onClick: () => props.onDelete(props.title),
+                }),
+            ],
+            styleClasses: ["task-row"],
+        });
+    }
+}
+
+class Checkbox extends Component {
+    constructor() {
+        super();
+        this.element = document.createElement(`input`);
+    }
+    render(props) {
+        this.element.type = "checkbox";
+        this.element.value = props.title;
+        this.element.onchange = () => {
+            if (this.element.checked) {
+                props.onChecked(props.title);
+            }
+        };
+        return super.render({
+            children: [],
+            styleClasses: ["checkbox"],
+        });
+    }
+}
+
+class DisabledCheckbox extends Checkbox {
+    render(props) {
+        this.element.checked = "true";
+        this.element.disabled = "true";
+        return super.render({
+            title: props.title,
+        });
+    }
+}
+
+class Label extends Component {
+    constructor() {
+        super();
+        this.element = document.createElement(`label`);
+    }
+    render(props) {
+        return super.render({
+            children: [props.title],
+            styleClasses: props.styleClasses,
+        });
+    }
+}
+
+class Image extends Component {
+    constructor() {
+        super();
+        this.element = document.createElement(`img`);
+    }
+    render(props) {
+        this.element.src = props.src;
+        return super.render({
+            children: [],
+            styleClasses: props.styleClasses,
+            onClick: props.onClick,
+        });
+    }
+}
+
+class DeleteIcon extends Image {
+    render(props) {
+        return super.render({
+            styleClasses: ["bucket-icon"],
+            onClick: props.onClick,
+            src: "bucket.svg",
+        });
+    }
+}
+
+class PopupContainer extends Component {
+    render(props) {
+        return super.render({
+            children: [
+                new Popup().render({
+                    onCancel: props.onCancel,
+                    onClickAdd: props.onClickAdd,
+                }),
+                new Overlay().render({}),
+            ],
+        });
+    }
+}
+class Popup extends Component {
+    render(props) {
+        return super.render({
+            children: [
+                new Header({ level: 1 }).render({
+                    text: "Add New Task",
+                }),
+                new Form().render({
+                    onCancel: props.onCancel,
+                    onClickAdd: props.onClickAdd,
+                }),
+            ],
+            styleClasses: ["popup"],
+        });
+    }
+}
+
+class Overlay extends Component {
+    render(props) {
+        return super.render({
+            children: [],
+            styleClasses: ["overlay"],
+        });
+    }
+}
+
+class Form extends Component {
+    constructor() {
+        super();
+        this.element = document.createElement(`form`);
+    }
+    render(props) {
+        this.element.onsubmit = (event) => {
+            event.preventDefault();
+            if (!taskInput.value) {
+                return;
+            }
+            props.onClickAdd(taskInput.value);
+        };
+        const taskInput = new InputText().render({
+            placeholder: "Task Title",
+            onInput: () => {
+                if (taskInput.value) {
+                    addButton.removeAttribute("disabled");
+                } else {
+                    addButton.disabled = "true";
+                }
+            },
+            value: "",
+            name: "taskTitle",
+            setFocus: true,
+        });
+        const [cancelButton, addButton] = [
+            new Button().render({
+                text: "Cancel",
+                onClick: props.onCancel,
+                styleClasses: ["cancel-btn"],
+                type: "reset",
+            }),
+            new Button().render({
+                text: "Add Task",
+                enabled: false,
+                styleClasses: ["add-task-btn", "confirm-btn"],
+                type: "submit",
+            }),
+        ];
+        return super.render({
+            children: [taskInput, cancelButton, addButton],
         });
     }
 }
