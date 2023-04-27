58b091c02ec193bf462b588d4b543d0dcf200328
agrinkevich <alina.grinkevich@quantori.academy>
2023-04-27 17:31:03 +0300

    TASK-6: Applying comment about BaseService

--- HW5/oop/services.js
+++ HW5/oop/services.js
@@ -1,4 +1,42 @@
+const TASK_SERVICE_BASE_URL = "http://localhost:3004";
+const WEATHER_SERVICE_BASE_URL = "http://api.weatherapi.com/v1";
+const WEATHER_SERVICE_API_KEY = "aaf607ad42a54fe4985111445231604";
+
 class BaseService {
+    constructor(baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    get(path) {
+        return fetch(`${this.baseUrl}/${path}`).then(this.handleResponse);
+    }
+
+    post(path, data) {
+        return fetch(`${this.baseUrl}/${path}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        }).then(this.handleResponse);
+    }
+
+    put(path, data) {
+        return fetch(`${this.baseUrl}/${path}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        }).then(this.handleResponse);
+    }
+
+    delete(path) {
+        return fetch(`${this.baseUrl}/${path}`, {
+            method: "DELETE",
+        }).then(this.handleResponse);
+    }
+
     handleResponse(response) {
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
@@ -9,52 +47,41 @@ class BaseService {
 }
 
 class TaskAppService extends BaseService {
-    BASE_URL = "http://localhost:3004";
-    TASKS_URL = `${this.BASE_URL}/tasks`;
+    TASK_API_PREFIX = "tasks";
+
+    constructor(baseUrl = TASK_SERVICE_BASE_URL) {
+        super(baseUrl);
+    }
 
     getTasks() {
-        return fetch(this.TASKS_URL).then(this.handleResponse);
+        return super.get(this.TASK_API_PREFIX);
     }
 
     createTask(task) {
-        return fetch(this.TASKS_URL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                title: task.title,
-                isCompleted: task.isCompleted,
-                plannedDate: task.plannedDate,
-            }),
-        }).then(this.handleResponse);
+        return super.post(this.TASK_API_PREFIX, {
+            title: task.title,
+            isCompleted: task.isCompleted,
+            plannedDate: task.plannedDate,
+        });
     }
 
     deleteTask(task) {
-        return fetch(`${this.TASKS_URL}/${task.id}`, {
-            method: "DELETE",
-        }).then(this.handleResponse);
+        return super.delete(`${this.TASK_API_PREFIX}/${task.id}`);
     }
 
     updateTask(task) {
-        return fetch(`${this.TASKS_URL}/${task.id}`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(task),
-        }).then(this.handleResponse);
+        return super.put(`${this.TASK_API_PREFIX}/${task.id}`, task);
     }
 }
 
 class WeatherService extends BaseService {
-    API_KEY = "aaf607ad42a54fe4985111445231604";
-    BASE_URL = "http://api.weatherapi.com/v1";
-    CURRENT_WEATHER_URL = `${this.BASE_URL}/current.json`;
+    constructor(baseUrl = WEATHER_SERVICE_BASE_URL) {
+        super(baseUrl);
+    }
 
     getWeather({ latitude, longitude }) {
-        return fetch(
-            `${this.CURRENT_WEATHER_URL}?key=${this.API_KEY}&q=${latitude},${longitude}`
-        ).then(this.handleResponse);
+        return super.get(
+            `current.json?key=${WEATHER_SERVICE_API_KEY}&q=${latitude},${longitude}`
+        );
     }
 }
